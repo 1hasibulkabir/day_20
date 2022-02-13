9b60a2da0195e82ed438e567c9b5f7cab4f85d67
shaown <shaownakter.1995@gmail.com>
2022-02-13 09:33:15 +0600

    day_20

---
+++ app/classes/Student.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\classes;
+
+
+class Student
+{
+protected $country = 'Bangladesh';
+public function manage()
+{
+   echo $this->country;
+}
+}
---
+++ app/classes/User.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\classes;
+
+
+class User
+{
+protected  $name = 'BITM';
+protected  $city = 'Dhaka';
+
+public function index()
+{
+    echo 'Institution name is '.$this->name.'and the city name is'.$this->city;
+}
+}
